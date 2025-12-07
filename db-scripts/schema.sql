9df7ba72489a59d8004d49f946d322ee830aa480
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2025-12-07 17:16:42 +0200

    Add 'checked' column to the entries schema

--- db-scripts/schema.sql
+++ db-scripts/schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE entries (
     id INTEGER PRIMARY KEY,
     position INTEGER NOT NULL CHECK (position >= 1),
-    content TEXT NOT NULL
+    content TEXT NOT NULL,
+    checked BOOLEAN NOT NULL DEFAULT 0 CHECK (checked IN (0, 1))
 );
