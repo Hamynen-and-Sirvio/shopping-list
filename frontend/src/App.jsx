54fa5de36fef17c0a014e55435d0c76355df8f02
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-02-06 15:31:42 +0200

    Fix entry editing size issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -201,66 +201,61 @@ const App = () => {
               <div
                 key={entry.id}
                 className="entry-container"
+                onClick={checkEntry(entry)}
               >
-                <div
-                  key={entry.id}
-                  className="list-entry"
-                  onClick={checkEntry(entry)}
-                >
-                  {entry.id === editEntryId ?
-                    <div className="entry-text-edit">
-                      <form onSubmit={editEntry(entry)} onClick={(e) => e.stopPropagation()}>
-                        <input
-                          value={editEntryField}
-                          onChange={event => setEditEntryField(event.target.value)}
-                          autoFocus
-                        />
-                        <button
-                          type="submit"
-                          className='save-button'
-                          onClick={(e) => e.stopPropagation()}
-                          hidden
-                        >
-                          Save
-                        </button>
-                      </form>
-                    </div> :
-                    <div className={`entry-text ${entry.checked ? 'checked' : ''}`}>
-                      {entry.content}
-                    </div>
-                  }
-                  {editMode && (
-                    <div className="edit-container" onClick={e => e.stopPropagation()}>
+                {entry.id === editEntryId ?
+                  <div className="entry-text-edit">
+                    <form onSubmit={editEntry(entry)} onClick={(e) => e.stopPropagation()}>
+                      <input
+                        value={editEntryField}
+                        onChange={event => setEditEntryField(event.target.value)}
+                        autoFocus
+                      />
                       <button
-                        type="button"
-                        className="entry-edit-button"
-                        onClick={
-                          editEntryId === -1
-                            ? selectEditEntry(entry)
-                            : cancelEditEntry(entry)
-                        }
+                        type="submit"
+                        className='save-button'
+                        onClick={(e) => e.stopPropagation()}
+                        hidden
                       >
-                        <LiaEditSolid />
+                        Save
                       </button>
-                      <button
-                        type="button"
-                        className="entry-edit-button"
-                        disabled={entry.position <= 1}
-                        onClick={moveEntry(entry, -1)}
-                      >
-                        <LiaChevronUpSolid />
-                      </button>
-                      <button
-                        type="button"
-                        className="entry-edit-button"
-                        disabled={entry.position >= entries.length}
-                        onClick={moveEntry(entry, 1)}
-                      >
-                        <LiaChevronDownSolid />
-                      </button>
-                    </div>
-                  )}
-                </div>
+                    </form>
+                  </div> :
+                  <div className={`entry-text ${entry.checked ? 'checked' : ''}`}>
+                    {entry.content}
+                  </div>
+                }
+                {editMode && (
+                  <div className="edit-container" onClick={e => e.stopPropagation()}>
+                    <button
+                      type="button"
+                      className="entry-edit-button"
+                      onClick={
+                        editEntryId === -1
+                          ? selectEditEntry(entry)
+                          : cancelEditEntry(entry)
+                      }
+                    >
+                      <LiaEditSolid />
+                    </button>
+                    <button
+                      type="button"
+                      className="entry-edit-button"
+                      disabled={entry.position <= 1}
+                      onClick={moveEntry(entry, -1)}
+                    >
+                      <LiaChevronUpSolid />
+                    </button>
+                    <button
+                      type="button"
+                      className="entry-edit-button"
+                      disabled={entry.position >= entries.length}
+                      onClick={moveEntry(entry, 1)}
+                    >
+                      <LiaChevronDownSolid />
+                    </button>
+                  </div>
+                )}
               </div>
             )}
           </div>
