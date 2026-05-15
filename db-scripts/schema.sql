4a7f42a8d53533a1e4876fb613465441866bdf6c
Ville Sirviö <VSirvio@users.noreply.github.com>
2026-05-15 17:07:30 +0300

    Delete 'db-scripts/schema.sql'

--- db-scripts/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE entries (
-    id INTEGER PRIMARY KEY,
-    position INTEGER NOT NULL CHECK (position >= 1),
-    content TEXT NOT NULL,
-    checked BOOLEAN NOT NULL DEFAULT 0 CHECK (checked IN (0, 1))
-);
