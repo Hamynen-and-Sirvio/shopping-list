3dcc9f86a3eb8d5d4cac694c5cf22ba1ad5d8c89
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-07-06 09:57:34 +0300

    Update UI

--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ const Loading = () => {
   return (
     <div className="loading-container">
       <div className="loading-spinner">
-        <PulseLoader color="#f9f9f9" speedMultiplier={0.5} size={8} />
+        <PulseLoader color="#ffffff" speedMultiplier={0.5} size={8} />
       </div>
     </div>
   )
