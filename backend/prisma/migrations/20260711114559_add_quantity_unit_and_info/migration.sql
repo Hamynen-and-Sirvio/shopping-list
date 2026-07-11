a46cf44c7fbd4ee590520dfa5567f35d8ecaca0e
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-07-11 15:25:25 +0300

    Update migration manually

--- backend/prisma/migrations/20260711114559_add_quantity_unit_and_info/migration.sql
+++ backend/prisma/migrations/20260711114559_add_quantity_unit_and_info/migration.sql
@@ -1,5 +1,7 @@
 -- AlterTable
-ALTER TABLE "Entry" RENAME CONSTRAINT "entries_pkey" TO "Entry_pkey",
+ALTER TABLE "Entry" RENAME CONSTRAINT "entries_pkey" TO "Entry_pkey";
+
+ALTER TABLE "Entry"
 ADD COLUMN     "additionalInfo" TEXT NOT NULL DEFAULT '',
 ADD COLUMN     "quantity" DOUBLE PRECISION NOT NULL DEFAULT 1,
 ADD COLUMN     "unit" TEXT NOT NULL DEFAULT 'kpl';
